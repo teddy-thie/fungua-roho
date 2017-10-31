2026bd1d4c20bc63218493ca006caa6a885514c3
Ted Thie <ted.thie@outlook.com>
2017-10-31 22:10:50 +0300

    Emmit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,11 +2,6 @@ var socket = io();
 
 socket.on('connect', function() {
     console.log('Connected to server');
-
-socket.emit('createMessage', {
-    from: 'ted.thie@outlook.com',
-    text: 'Nyanyako'
-});
 });
 
 socket.on('disconnect', function()  {
