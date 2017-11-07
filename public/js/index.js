aac0f503008576184648986b224173048ec17cc4
Ted Thie <ted.thie@outlook.com>
2017-11-08 01:39:31 +0300

    We have added the timestamps to the chat application

--- public/js/index.js
+++ public/js/index.js
@@ -9,27 +9,28 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-  console.log('newMessage', message);
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  li.text(`${message.from}: ${message.text}`);
+  li.text(`${message.from} ${formattedTime}: ${message.text}`);
 
   jQuery('#messages').append(li);
 });
 
-socket.on('newLocationMessage', function (message){
+socket.on('newLocationMessage', function (message) {
+  var formattedTime = moment(message.createdAt).format('h:mm a');
   var li = jQuery('<li></li>');
-  var a = jQuery('<a target="_blank">My current Location</a>');
+  var a = jQuery('<a target="_blank">My current location</a>');
 
-  li.text(`${message.from}: `);
+  li.text(`${message.from} ${formattedTime}: `);
   a.attr('href', message.url);
   li.append(a);
   jQuery('#messages').append(li);
-})
+});
 
 jQuery('#message-form').on('submit', function (e) {
   e.preventDefault();
 
-var messageTextbox = jQuery('[name=message]');
+  var messageTextbox = jQuery('[name=message]');
 
   socket.emit('createMessage', {
     from: 'User',
@@ -45,7 +46,7 @@ locationButton.on('click', function () {
     return alert('Geolocation not supported by your browser.');
   }
 
-locationButton.attr('disabled','disabled').text('Sending location...');
+  locationButton.attr('disabled', 'disabled').text('Sending location...');
 
   navigator.geolocation.getCurrentPosition(function (position) {
     locationButton.removeAttr('disabled').text('Send location');
