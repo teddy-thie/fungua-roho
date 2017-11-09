110cb087df09bbf541ab905bfa9b2f07e4568f45
Ted Thie <ted.thie@outlook.com>
2017-11-09 22:42:49 +0300

    Finishing up

--- server/utils/message.js
+++ server/utils/message.js
@@ -1,18 +1,19 @@
+var moment = require('moment');
+
 var generateMessage = (from, text) => {
-    return {
-      from,
-      text,
-      createdAt: new Date().getTime()
-    };
+  return {
+    from,
+    text,
+    createdAt: moment().valueOf()
   };
-  
-  var generateLocationMessage = (from, latitude, longitude) => {
-    return {
-      from,
-      url: `https://www.google.com/maps?q=${latitude},${longitude}`,
-      createdAt: new Date().getTime()
-    };
+};
+
+var generateLocationMessage = (from, latitude, longitude) => {
+  return {
+    from,
+    url: `https://www.google.com/maps?q=${latitude},${longitude}`,
+    createdAt: moment().valueOf()
   };
-  
-  module.exports = {generateMessage, generateLocationMessage};
-  
+};
+
+module.exports = {generateMessage, generateLocationMessage};
